2986835627762cedf8e9fec806852bbd2a34a442
Jackson Singleton <jsingleton12321@yahoo.com>
2018-09-25 18:28:03 -0500

    Fixed DrawingPanel Errors

--- Unorganized/tester.java
+++ Unorganized/tester.java
@@ -1,19 +1,6 @@
-public class Tester{
-    public static void main(String[] args){
-        printGrid(4, 6);
-    }
-    public static void printGrid(int a, int b){
-        for (int i = 1; i <= a+1; i++) {   
-            for (int j = 1; j <= b;j++) {
-               System.out.print(i+(j*a)-a);
-               for (int k = 0; k < 1; k++) {
-                System.out.print(", ");
-               }
-            }
-            
-            
-            System.out.println();
-        }
-        
+
+public class Tester {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
     }
 }
