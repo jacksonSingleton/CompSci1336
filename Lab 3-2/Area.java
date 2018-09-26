b9b6ece848df3e398e88d7a5b4c6d1b527925cb3
Jackson Singleton <jsingleton12321@yahoo.com>
2018-09-25 19:10:34 -0500

    Added some doc

--- Lab 3-2/Area.java
+++ Lab 3-2/Area.java
@@ -6,6 +6,10 @@ public class Area {
         //Replace the argument of the function to change the radius
         area(2.0);   
     }
+    //Method is a double instead of void so it can 
+    //return a double
+    //i.e if you want to return an int use:
+    //public static int methodName(arguments){}
     public static double area(double radius){
             double areaCalc = Math.PI*Math.pow(radius,2);
             return areaCalc;
